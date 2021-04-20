8878e7f4f14ebfe973556c32aba6ed4dba13f0f2
karan yadav <47856276+karan68@users.noreply.github.com>
2021-04-20 21:21:21 +0530

    Update Activity.cs

--- src/Modules/SimplCommerce.Module.ActivityLog/Models/Activity.cs
+++ src/Modules/SimplCommerce.Module.ActivityLog/Models/Activity.cs
@@ -6,13 +6,16 @@ namespace SimplCommerce.Module.ActivityLog.Models
 {
     public class Activity : EntityBase
     {
+    
         public long ActivityTypeId { get; set; }
 
         public ActivityType ActivityType { get; set; }
 
         public long UserId { get; set; }
-
-        public DateTimeOffset CreatedOn { get; set; }
+        
+       [Display(Name = "Created On")]
+        public DateTimeOffset CreatedOn { get; set; } //The DateTimeOffset structure includes a DateTime value, together with an Offset property that defines the difference between the current DateTimeOffset instance's date and time and Coordinated Universal Time (UTC). 
+        //Because it exactly defines a date and time relative to UTC, the DateTimeOffset structure does not include a Kind member, as the DateTime structure does.
 
         public long EntityId { get; set; }
 
